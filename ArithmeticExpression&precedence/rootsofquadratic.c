492d422a67c33e40c5185a43bbaf7e6c9ff67a3c
Sayantan <sayantanhalder43@gmail.com>
2025-04-21 21:55:33 +0530

    First commit

---
+++ ArithmeticExpression&precedence/rootsofquadratic.c
@@ -0,0 +1,9 @@
+// Write a program to find the roots of quadratic equation
+#include <complex.h>
+#include <math.h>
+#include <stdio.h>
+int main()
+{
+    double a, b, c, discriminant, root1, root2, realpart, imgpart;
+    
+}
