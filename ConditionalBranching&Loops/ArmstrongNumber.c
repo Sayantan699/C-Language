bad14988289c4bc770223a077000765ad36872bd
Sayantan <sayantanhalder43@gmail.com>
2025-04-26 23:24:35 +0530

    First commit

---
+++ ConditionalBranching&Loops/ArmstrongNumber.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+    int num=n;
+    int sum;
+    while(num>0){
+        int temp = num %10;
+        sum +=  temp * temp * temp;
+        num /= 10;
+    }
+
+    if(sum == n){
+        printf("Its an armstrong number");
+    }
+    else{
+        printf("Its not an armstrong number");
+    }
+
+    return 0;
+}
